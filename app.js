b822ee7bd5a7625a21b208c6a943268a236742cd
Jose Lopez <jflo1937@gmail.com>
2023-10-08 23:58:01 -0400

    Agregando archivos modifiacados en node_modules (no se por que), Probando Jimp y subida de archivos y creacion de carpetas mipmap para android

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const bodyParser = require('body-parser');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -17,6 +19,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter); // rutas para pruebas
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,35 @@
+// Pasos para construir el algritmo
+// 1. Formulario para donde se obtenga el nombre de la app y el archivo de imagen para usarse como icono
+// 1.2. Copiar el proyecto base de RN entero para hacerle las modificaciones pertinentes
+// 1.3. Reemplazar la url del webview por la del formulario
+// 1.4. Buscar el archivo del nomrbe de la app y Renombrarlo por el del formulario
+// 1.5. Buscar todos los archivos del nombre de paquete y renombrarlo a uno que coincida con el del nombre de la app o seleccionarlo del formulario
+// 1.6. Modificar el tamano del icono y agregarlo a la carpeta assets de la app
+// 1.7.
+// 2. Compilar la app e ir retornando el output
+// 2.1. descargar el archivo compilado
+// 2.2. Eliminar el proyecto al cabo de cierto tiempo
+
 const express = require('express');
 const { exec } = require('child_process');
 const fs = require('fs');
-const https = require("https");
+const Jimp = require('jimp');
+
+const upload = require('../middlewares/upload');
 
 var router = express.Router();
 
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/listar', function(req, res, next) {
-  const ls = fs.readdirSync('webview/android');
-  console.log(JSON.stringify(ls, null, 4));
-  
-  res.json(ls);  
-});
-
-router.get('/update-file', function(req, res, next) {
-  // Leer el contenido del archivo
-
-  const rutaArchivo = "./project/App.tsx";
-
-  fs.readFile(rutaArchivo, 'utf8', (err, data) => {
+const updateFileContent = (filePath, earlierText, newText) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       console.error('Error al leer el archivo:', err);
-    } else {
+    }
+    else {
       // Reemplazar la palabra antigua por la nueva en el contenido
-      const contenidoModificado = data.replace('https://www.youtube.com/', 'https://www.mercadolibre.com.ve/');
+      const contenidoModificado = data.replace(earlierText, newText);
 
       // Escribir el contenido modificado en el archivo
-      fs.writeFile(rutaArchivo, contenidoModificado, 'utf8', (err) => {
+      fs.writeFile(filePath, contenidoModificado, 'utf8', (err) => {
         if (err) {
           console.error('Error al escribir en el archivo:', err);
         } else {
@@ -40,18 +38,76 @@ router.get('/update-file', function(req, res, next) {
       });
     }
   })
+}
+
+/* PROCESS FORM. */
+router.post('/process-app', function(req, res, next) {
+  const name = req.body.name;
+  const package = req.body.package || `com.lopez_app_generator.${name}`;
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'López App Generator' });
 });
 
 router.get('/listar', function(req, res, next) {
-  const ls = fs.readdirSync('webview/android');
+  const ls = fs.readdirSync('project/android');
   console.log(JSON.stringify(ls, null, 4));
-  
-  res.json(ls);  
+
+  res.json(ls);
+});
+
+router.post('/update-file', upload.single('icon'), async (req, res, next) => {
+  const mipmap_folders = [
+    {
+      name: 'mipmap-hdpi',
+      fileDimension: 72
+    },
+    {
+      name: 'mipmap-mdpi',
+      fileDimension: 48
+    },
+    {
+      name: 'mipmap-xhdpi',
+      fileDimension: 96
+    },
+    {
+      name: 'mipmap-xxhdpi',
+      fileDimension: 144
+    },
+    {
+      name: 'mipmap-xxxhdpi',
+      fileDimension: 192
+    },
+  ]
+
+  // const name = req.body.name;
+  const file = req.file;
+  // const rutaArchivo = "./project/App.tsx";
+  // const earlierText = "";
+  // const newText = "";
+
+  // Crear iconos con nombre ic_launcher en las carpetas mipmap con sus respectivas dimensiones
+  mipmap_folders.map(async (item) => {
+    await Jimp.read(file.buffer, (err, image) => {
+      if (err) res.status(500).send(err);
+
+      image
+        .cover(item.fileDimension, item.fileDimension)
+        .quality(50)
+        .write(`uploads/${item.name}/ic_launcher.png`);
+
+      res.end();
+    });
+  });
+
+  // updateFileContent(rutaArchivo, earlierText, newText);
 });
 
 router.get('/download', function(req, res, next) {
   const ruta_apk = "./project/android/app/build/outputs/apk/release/app-release.apk";
-  
+
   fs.access(ruta_apk, fs.constants.F_OK, (err) => {
     if (!err) {
       fs.stat(ruta_apk, (err, stats) => {
@@ -68,7 +124,7 @@ router.get('/download', function(req, res, next) {
   });
 });
 
-router.get('/build', function(req, res, next) {  
+router.get('/build', function(req, res, next) {
   const child = exec('cd project && yarn bundle');
 
   // Mostrar el output a medida que se va ejecutando
@@ -87,5 +143,5 @@ router.get('/build', function(req, res, next) {
   });
 
 });
- 
+
 module.exports = router;
